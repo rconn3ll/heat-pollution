d2ae66a56ed91e40f681a7dfcd5fc5293051645f
Robbie <31318377+rconn3ll@users.noreply.github.com>
2023-05-05 18:40:35 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const images = ['images/Heat_Pollution_p1.png','images/Heat_Pollution_p2.png','images/Heat_Pollution_p3.png','images/Heat_Pollution_p4.png'];
+let index = 0;
+
+window.addEventListener('load', () => {
+    document.addEventListener('click', () => {
+        next_img();
+    }); 
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            next_img();
+        }
+    })
+});
+
+function next_img(){
+    if (index < images.length-1) index++;
+    else index = 0;
+    document.getElementById('img').src = images[index];
+}
